05f51c61ed0719863fb556a811e17f3e7ff952e2
Roenan Halili <haliliroenan12@gmail.com>
2025-02-15 15:03:43 +0800

    refactor: extract budget categories to const

--- components/BudgetsForm.tsx
+++ components/BudgetsForm.tsx
@@ -12,18 +12,7 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { createBudget } from "@/lib/actions";
-
-const budgetCategories = [
-  "Food (Groceries, Restaurants, etc.)",
-  "Utilities (Bills, Internet, etc.)",
-  "Shopping (Clothing, Accessories, etc.)",
-  "Personal Care (Haircuts, Gym, etc.",
-  "Entertainment (Movies, Video Games, etc.)",
-  "Transportation (Gas, Public Transit, etc.)",
-  "Travel (Flights, Hotels, etc.)",
-  "Investments (Stocks, Retirement, etc.)",
-  "Other (Miscellaneous, Debt Payments, etc.)",
-];
+import { budgetCategories } from "@/lib/constants";
 
 function BudgetsForm() {
   const [name, setName] = useState("");
---
+++ lib/constants.ts
@@ -0,0 +1,12 @@
+
+export const budgetCategories = [
+  "Food (Groceries, Restaurants, etc.)",
+  "Utilities (Bills, Internet, etc.)",
+  "Shopping (Clothing, Accessories, etc.)",
+  "Personal Care (Haircuts, Gym, etc.",
+  "Entertainment (Movies, Video Games, etc.)",
+  "Transportation (Gas, Public Transit, etc.)",
+  "Travel (Flights, Hotels, etc.)",
+  "Investments (Stocks, Retirement, etc.)",
+  "Other (Miscellaneous, Debt Payments, etc.)",
+];
