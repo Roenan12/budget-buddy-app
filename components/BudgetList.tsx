0e685d2b20eff3a0adc12862a60ff1a68c46eba2
Roenan Halili <haliliroenan12@gmail.com>
2025-02-17 23:53:13 +0800

    refactor: remove async in function

--- components/BudgetList.tsx
+++ components/BudgetList.tsx
@@ -5,7 +5,7 @@ type BudgetListProps = {
   budgets: Budget[];
 };
 
-async function BudgetList({ budgets }: BudgetListProps) {
+function BudgetList({ budgets }: BudgetListProps) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
       {budgets.map((budget) => (
