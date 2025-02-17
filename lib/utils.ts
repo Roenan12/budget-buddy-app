96bcaf992d4a7c95d0b22a0be980a3940b41726a
Roenan Halili <haliliroenan12@gmail.com>
2025-02-17 20:23:33 +0800

    feat: add util getErrorMessage function for error handling

--- lib/utils.ts
+++ lib/utils.ts
@@ -12,3 +12,20 @@ export function formatDate(date: string | Date): string {
     day: "numeric",
   });
 }
+
+// example usage in try catch ( getErrorMessage(error) )
+export const getErrorMessage = (error: unknown): string => {
+  let message: string;
+
+  if (error instanceof Error) {
+    message = error.message;
+  } else if (error && typeof error === "object" && "message" in error) {
+    message = String(error.message);
+  } else if (typeof error === "string") {
+    message = error;
+  } else {
+    message = "Something went wrong";
+  }
+
+  return message;
+};
